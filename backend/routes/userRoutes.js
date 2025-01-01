4446441591661794553258bf0eaf899094c917a3
Çağrıhan GÜNAY <chgunay3@gmail.com>
2025-01-01 16:33:26 +0300

    chore: Update userRoutes.js to use destructured import for authMiddleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const { verifyToken } = require('../middlewares/authMiddleware');
-const { isAdmin } = require('../middlewares/authMiddleware');
+const { verifyToken, isAdmin } = require('../middlewares/authMiddleware');
 
-// Sadece admin kullanıcılar user CRUD yapabilsin:
 router.get('/', verifyToken, isAdmin, userController.getAllUsers);
 router.get('/:userId', verifyToken, isAdmin, userController.getUser);
 router.put('/:userId', verifyToken, isAdmin, userController.updateUser);
