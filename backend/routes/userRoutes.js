b79a536dbe73bd7fc6c9dc2b3145a5e81578b508
Çağrıhan GÜNAY <chgunay3@gmail.com>
2025-01-01 15:42:33 +0300

    feat: Add isAdmin middleware for admin authorization

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,13 @@
-
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const { verifyToken } = require('../middlewares/authMiddleware');
+const { isAdmin } = require('../middlewares/authMiddleware');
 
-// Sadece login olmuş kullanıcılar (JWT Token) işlem yapabilsin
-router.get('/', verifyToken, userController.getAllUsers);
-router.get('/:userId', verifyToken, userController.getUser);
-router.put('/:userId', verifyToken, userController.updateUser);
-router.delete('/:userId', verifyToken, userController.deleteUser);
+// Sadece admin kullanıcılar user CRUD yapabilsin:
+router.get('/', verifyToken, isAdmin, userController.getAllUsers);
+router.get('/:userId', verifyToken, isAdmin, userController.getUser);
+router.put('/:userId', verifyToken, isAdmin, userController.updateUser);
+router.delete('/:userId', verifyToken, isAdmin, userController.deleteUser);
 
 module.exports = router;
