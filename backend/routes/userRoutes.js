6b007e106079b3572426891b1a52d1b586524119
Çağrıhan GÜNAY <chgunay3@gmail.com>
2025-01-02 20:50:51 +0300

    chore: Update userRoutes.js to use userGatewayController for user creation

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const { verifyToken, isAdmin } = require('../middlewares/authMiddleware');
-
+const userGatewayController = require('../controllers/userGatewayController');
 
 router.get('/', verifyToken, isAdmin, userController.getAllUsers);
 router.get('/:userId', verifyToken, isAdmin, userController.getUser);
 
-router.post('/', verifyToken, isAdmin, userController.createUser);
+router.post('/', verifyToken, isAdmin, userGatewayController.createUser);
+//router.post('/', verifyToken, isAdmin, userController.createUser);
 router.put('/:userId', verifyToken, isAdmin, userController.updateUser);
 router.delete('/:userId', verifyToken, isAdmin, userController.deleteUser);
 
