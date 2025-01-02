9d5bd0b2099d65e3e1a358c825d9aaf79d5eb5ed
Çağrıhan GÜNAY <chgunay3@gmail.com>
2025-01-02 20:50:39 +0300

    chore: Update taskRoutes.js to use taskGatewayController for task creation

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/taskController');
 const { verifyToken } = require('../middlewares/authMiddleware');
+const taskGatewayController = require('../controllers/taskGatewayController');
 
-router.post('/', verifyToken, taskController.createTask);
+router.post('/', verifyToken, taskGatewayController.createTask);
+
+//direct db write
+//router.post('/', verifyToken, taskController.createTask);
 router.get('/', verifyToken, taskController.getAllTasks);
 router.get('/:taskId', verifyToken, taskController.getTask);
 router.put('/:taskId', verifyToken, taskController.updateTask);
