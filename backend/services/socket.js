5c7b21f63b428b48fff23f88b8738e7b9db52cbb
Çağrıhan GÜNAY <chgunay3@gmail.com>
2025-01-02 20:52:09 +0300

    chore: Refactor socket.js to use clientSocket instead of socket for consistency and clarity

--- backend/services/socket.js
+++ backend/services/socket.js
@@ -2,23 +2,30 @@ let io;
 
 module.exports = {
   init: (server) => {
-    // Socket.IO'yu başlat
     io = require('socket.io')(server, {
       cors: {
-        origin: '*', 
+        origin: '*',
       },
     });
 
-    // Bağlantı gerçekleştiğinde
-    io.on('connection', (socket) => {
-      console.log('Bir kullanıcı bağlandı. Socket ID:', socket.id);
+    io.on('connection', (clientSocket) => {
+      console.log('Bir kullanıcı (veya Worker) bağlandı. Socket ID:', clientSocket.id);
 
-      socket.on('disconnect', () => {
-        console.log('Kullanıcı ayrıldı. Socket ID:', socket.id);
+      clientSocket.on('taskCreated', (newTaskData) => {
+        console.log('Worker’dan bir "taskCreated" event geldi ->', newTaskData);
+        io.emit('taskCreated', newTaskData);
+      });
+
+      clientSocket.on('userCreated', (newUserData) => {
+        console.log('Worker’dan bir "userCreated" event geldi ->', newUserData);
+        io.emit('userCreated', newUserData);
+      });
+
+      clientSocket.on('disconnect', () => {
+        console.log('Kullanıcı ayrıldı. Socket ID:', clientSocket.id);
       });
     });
   },
 
-  // Projenin diğer yerlerinden io nesnesine erişmek için
-  getIO: () => io
+  getIO: () => io,
 };
